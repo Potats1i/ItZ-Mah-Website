5eb9e319a6c28f4e3b09324255f1e8fbc3a4009d
Potats1i <140161982+Potats1i@users.noreply.github.com>
2023-07-29 03:24:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // script
 console.log("Hello world");
-document.getElementById("btn1").onclick = function() {
-  console.log("button 1 is pressed")
-  document.getElementById("head").style.color = "red";
-}
-document.getElementById("btn2").onclick = function() {
-  console.log("button 2 is pressed")
-  document.getElementById("head").style.color = "#00AA00";
-}
-document.getElementById("btn3").onclick = function() {
-  console.log("button 3 is pressed")
-  document.getElementById("h3").remove();
-}
 document.getElementById("sub").onclick = function() {
   alert("GIGACHAD APPROVED THIS WEBSITE")
 }
+//end
